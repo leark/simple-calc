a097c74b1494f254b7e2bafd9a9d874314deaf2c
Seung Lee <shl7@uw.edu>
2016-10-04 20:24:50 -0700

    calculator functions complete

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -8,8 +8,65 @@
 
 import Foundation
 
-print("Hello, World!")
+func factorial(number: Int) -> Int {
+    if number == 0 {
+        return 1;
+    } else {
+        return number * factorial(number: number - 1)
+    }
+}
 
+print("Enter an expression separated by returns:")
+
+var ints = [Int]()
+
+var response = readLine(strippingNewline: true)!
+var num = Int.init(response)
+while num != nil {
+    ints.append(num!)
+    response = readLine(strippingNewline: true)!
+    num = Int.init(response)
+}
+
+let operand = response
+
+var response2:String
+var num2 = 0
+if ints.count == 1 && operand != "fact" && operand != "count" && operand != "avg" {
+    response2 = readLine(strippingNewline: true)!
+    num2 = Int(UInt.init(response2)!)
+}
+
+var result = 0
+switch operand {
+case "+":
+    result = ints[0] + num2
+case "-":
+    result = ints[0] + num2
+case "*":
+    result = ints[0] * num2
+case "/":
+    result = ints[0] / num2
+case "%":
+    result = ints[0] % num2
+case "count":
+    result = ints.count
+case "avg":
+    for stored in ints {
+        result += stored
+    }
+    result = result / ints.count
+case "fact":
+    if ints.count == 1 {
+        result = factorial(number: ints[0])
+    } else {
+        print("Can only accept one number for factorial")
+    }
+default:
+    print("unknown operand")
+}
+
+print("Result: \(result)")
 /*
  enter a number hit return
  enter operation (add, sub, mul, div, mod) then return
@@ -36,30 +93,3 @@ print("Hello, World!")
  
  */
 
-func add() -> Int {
-    return 5
-}
-
-func sub() -> Int {
-    return 6
-}
-
-func mul() -> Int {
-    return 7
-}
-
-func div() -> Int {
-    return 8
-}
-
-func fact() -> Int {
-    return 9
-}
-
-func count() -> Int {
-    return 10
-}
-
-func avg() -> Int {
-    return 11
-}
